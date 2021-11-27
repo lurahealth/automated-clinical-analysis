3d50856654dd8d0a8315c76c97d35556a9cd989c
Noah <noah@lurahealth.com>
2021-11-27 03:46:10 -0500

    parse through and match all csv files in directory, append them next up

--- automated-analysis.py
+++ automated-analysis.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import csv
 import os
+from pathlib import Path
 
     # Parses file stored at "path" that describes patient ID <> device ID 
     # relationships. Returns an array of patient IDs and device IDs, such
@@ -20,7 +21,25 @@ def parse_ID_relations_file(path):
     
     return patient_ids, device_ids
 
-
+def collect_patient_csv_files(patient_ids, device_ids):
+    path = "../lura_files/" # this is bad but easier to test across hardwares
+    id_prefix = path + "ble-receiver"
+    tablet_id = "clinical-tabletA"     # Make sure to check the tablet too :)
+    for patient_id in patient_ids:
+        # search ph receiver directory for all active pH retainers
+        for device_id in device_ids:
+            device_id = "LuraHealth" + device_id + ".csv"
+            filename = id_prefix + patient_id + "/csv_files/"
+            complete_filename = filename + device_id
+            # print("complete FILENAME: ", complete_filename)
+            patient_dir = os.listdir(id_prefix + patient_id + "/csv_files")
+            print("Patient dir: ", patient_dir)
+            print("Device id: " , device_id)
+            
+            if device_id in patient_dir:
+                # print(complete_filename)
+                print("yeaaah buddy lmao")
+                # do something lol fuck
 
 
 def main(argv):
